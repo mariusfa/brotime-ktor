f2af8998c26f889b5b37e6e72eecf74482f035ad
mariusfa <mariusfagerland@yahoo.no>
2022-06-26 20:56:52 +0200

    Change to EngineMain

--- src/main/kotlin/fagerland/Application.kt
+++ src/main/kotlin/fagerland/Application.kt
@@ -1,17 +1,11 @@
-package fagerland
+package com.example
 
+import configureRouting
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!")
-            }
-        }
-    }.start(wait = true)
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    configureRouting()
 }
