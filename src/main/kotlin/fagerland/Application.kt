b37af48a26aa0448ca6340714d2bf4a5745f582d
mariusfa <mariusfagerland@yahoo.no>
2022-07-04 14:35:14 +0200

    Set cors

--- src/main/kotlin/fagerland/Application.kt
+++ src/main/kotlin/fagerland/Application.kt
@@ -3,10 +3,14 @@ package fagerland
 import configureRouting
 import fagerland.plugins.configureSerialization
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
+    install(CORS) {
+        anyHost()
+    }
     configureRouting()
     configureSerialization()
 }
